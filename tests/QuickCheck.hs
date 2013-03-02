0aa3da49bd0a7003d65b33206a2b61148938338a
Seth Fowler <mark.seth.fowler@gmail.com>
2013-03-02 12:52:02 -0800

    Added QuickCheck

---
+++ tests/QuickCheck.hs
@@ -0,0 +1,20 @@
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck hiding ((.&&.))
+import Test.QuickCheck.Function
+
+import Data.Bool.Predicate
+
+main:: IO ()
+main = defaultMain tests
+
+tests =
+  [
+    testGroup "Data.Bool.Predicate"
+    [
+      testProperty "Predicate Composition" prop_predicate_composition
+    ]
+  ]
+
+prop_predicate_composition :: (Fun Int Bool) -> (Fun Int Bool) -> Int -> Bool
+prop_predicate_composition (Fun _ f) (Fun _ g) v = ((f .&&. g) v) == ((f v) && (g v))
