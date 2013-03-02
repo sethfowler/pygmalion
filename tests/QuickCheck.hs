fd2597337996f876ed62cec8e90eb728e8bdd70e
Seth Fowler <mark.seth.fowler@gmail.com>
2013-03-02 13:22:05 -0800

    Removed unnecessary import

--- tests/QuickCheck.hs
+++ tests/QuickCheck.hs
@@ -1,6 +1,5 @@
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.QuickCheck hiding ((.&&.))
 import Test.QuickCheck.Function
 
 import Data.Bool.Predicate
