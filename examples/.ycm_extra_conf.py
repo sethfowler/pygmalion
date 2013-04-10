b999fcf1bdcf5085dcc6ba0cc1975f1d4e38efb4
Seth Fowler <seth@mozilla.com>
2013-04-09 21:22:29 -0700

    Update YCM config example

--- examples/.ycm_extra_conf.py
+++ examples/.ycm_extra_conf.py
@@ -1,47 +1,10 @@
 import os
-import ycm_core
-from clang_helpers import PrepareClangFlags
-
-def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
-
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return flags
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
-
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
-
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
-
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
-
 
 def FlagsForFile( filename ):
-  relative_to = DirectoryOfThisScript()
-  cmd = 'pygmalion --flags-for-file ' + os.path.relpath(filename)
+  cmd = 'pygmalion --flags-for-file ' + filename
   pygflags = os.popen(cmd).read().strip().split(' ')
-  final_flags = MakeRelativePathsInFlagsAbsolute( pygflags, relative_to )
 
   return {
-    'flags': final_flags,
+    'flags': pygflags
     'do_cache': True
   }
