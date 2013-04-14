4f2ed124ca13b82a7642d57c1e991435e9f0c595
Seth Fowler <mark.seth.fowler@gmail.com>
2013-04-13 18:10:25 -0700

    Use hash keys for further database performance improvements

--- src/Pygmalion/Database.hs
+++ src/Pygmalion/Database.hs
@@ -16,6 +16,7 @@ module Pygmalion.Database
 import Control.Applicative
 import Control.Exception(bracket)
 import Control.Monad
+import Data.Hashable
 import Data.Int
 import Data.String
 import qualified Data.Text as T
@@ -32,15 +33,10 @@ data DBHandle = DBHandle {
                   conn :: Connection,
                   updateSourceFileStmt :: Statement,
                   updateDefStmt :: Statement,
-                  getFileStmt :: Statement,
                   insertFileStmt :: Statement,
-                  getPathStmt :: Statement,
                   insertPathStmt :: Statement,
-                  getCommandStmt :: Statement,
                   insertCommandStmt :: Statement,
-                  getArgsStmt :: Statement,
                   insertArgsStmt :: Statement,
-                  getKindStmt :: Statement,
                   insertKindStmt :: Statement
                 }
 
@@ -55,19 +51,14 @@ openDB db = labeledCatch "openDB" $ do
   c <- open db
   disableSynchronousWrites c
   enableMemoryJournaling c
-  enableForeignKeyConstraints c
+  -- enableForeignKeyConstraints c
   ensureSchema c
   h <- DBHandle c <$> openStatement c (mkQueryT updateSourceFileSQL)
                   <*> openStatement c (mkQueryT updateDefSQL)
-                  <*> openStatement c (mkQueryT getFileSQL)
                   <*> openStatement c (mkQueryT insertFileSQL)
-                  <*> openStatement c (mkQueryT getPathSQL)
                   <*> openStatement c (mkQueryT insertPathSQL)
-                  <*> openStatement c (mkQueryT getCommandSQL)
                   <*> openStatement c (mkQueryT insertCommandSQL)
-                  <*> openStatement c (mkQueryT getArgsSQL)
                   <*> openStatement c (mkQueryT insertArgsSQL)
-                  <*> openStatement c (mkQueryT getKindSQL)
                   <*> openStatement c (mkQueryT insertKindSQL)
   beginTransaction h
   return h
@@ -77,15 +68,10 @@ closeDB h = do
   endTransaction h
   closeStatement (updateSourceFileStmt h)
   closeStatement (updateDefStmt h)
-  closeStatement (getFileStmt h)
   closeStatement (insertFileStmt h)
-  closeStatement (getPathStmt h)
   closeStatement (insertPathStmt h)
-  closeStatement (getCommandStmt h)
   closeStatement (insertCommandStmt h)
-  closeStatement (getArgsStmt h)
   closeStatement (insertArgsStmt h)
-  closeStatement (getKindStmt h)
   closeStatement (insertKindStmt h)
   close (conn h)
 
@@ -110,8 +96,15 @@ endTransaction h = execute_ (conn h) "end transaction"
 voidNextRow :: Statement -> IO (Maybe (Only Int64))
 voidNextRow = nextRow
 
-voidWithBind :: ToRow params => Statement -> params -> IO () -> IO ()
-voidWithBind s p a = void $ withBind s p a
+execStatement :: ToRow a => DBHandle -> (DBHandle -> Statement) -> a -> IO ()
+execStatement h q params  = void $ withBind stmt params $ (voidNextRow stmt)
+  where stmt = q h
+
+mkQuery :: String -> Query
+mkQuery = fromString
+
+mkQueryT :: T.Text -> Query
+mkQueryT = mkQuery . T.unpack
 
 -- Schema and operations for the Metadata table.
 dbToolName :: String
@@ -119,14 +112,14 @@ dbToolName = "pygmalion"
 
 dbMajorVersion, dbMinorVersion :: Int64
 dbMajorVersion = 0
-dbMinorVersion = 5
+dbMinorVersion = 6
 
 defineMetadataTable :: Connection -> IO ()
-defineMetadataTable c = execute_ c 
-  "create table if not exists Metadata(               \
-  \ Tool varchar(2048) primary key not null,          \
-  \ MajorVersion integer zerofill unsigned not null,  \
-  \ MinorVersion integer zerofill unsigned not null)"
+defineMetadataTable c = execute_ c sql
+  where sql = "create table if not exists Metadata(               \
+              \ Tool varchar(2048) primary key not null,          \
+              \ MajorVersion integer zerofill unsigned not null,  \
+              \ MinorVersion integer zerofill unsigned not null)"
 
 getDBVersion :: Connection -> IO (Maybe (Int64, Int64))
 getDBVersion c = do
@@ -135,161 +128,114 @@ getDBVersion c = do
               [version] -> Just version
               _         -> Nothing
   where sql = "select MajorVersion, MinorVersion from Metadata \
-              \where Tool = ?"
+              \ where Tool = ?"
         params = Only dbToolName
 
 setDBVersion :: Connection -> IO ()
 setDBVersion c = execute c sql params
-  where sql = "insert into Metadata (Tool, MajorVersion, MinorVersion) \
-              \values (?, ?, ?)"
+  where sql =  "insert into Metadata (Tool, MajorVersion, MinorVersion) \
+              \ values (?, ?, ?)"
         params = (dbToolName, dbMajorVersion, dbMinorVersion)
 
 -- Schema and operations for the Files table.
 defineFilesTable :: Connection -> IO ()
-defineFilesTable c = execute_ c sql
-  where sql =  "create table if not exists Files(        \
+defineFilesTable c = execute_ c sql >> execute_ c sqlIdx
+  where sql = "create table if not exists Files(         \
                \ Id integer primary key unique not null, \
+               \ Hash integer not null,                  \
                \ Name varchar(2048) unique not null)"
+        sqlIdx = "create index if not exists FilesHash on Files(Hash)"
+
+insertFileSQL :: T.Text
+insertFileSQL = "insert or ignore into Files (Name, Hash) values (?, ?)"
 
 -- Schema and operations for the Paths table.
 definePathsTable :: Connection -> IO ()
-definePathsTable c = execute_ c sql
+definePathsTable c = execute_ c sql >> execute_ c sqlIdx
   where sql =  "create table if not exists Paths(        \
                \ Id integer primary key unique not null, \
+               \ Hash integer not null,                  \
                \ Path varchar(2048) unique not null)"
+        sqlIdx = "create index if not exists PathsHash on Paths(Hash)"
+
+insertPathSQL :: T.Text
+insertPathSQL = "insert or ignore into Paths (Path, Hash) values (?, ?)"
 
 -- Schema and operations for the BuildCommands table.
 defineBuildCommandsTable :: Connection -> IO ()
-defineBuildCommandsTable c = execute_ c sql
-  where sql =  "create table if not exists BuildCommands(\
-               \ Id integer primary key unique not null, \
+defineBuildCommandsTable c = execute_ c sql >> execute_ c sqlIdx
+  where sql =  "create table if not exists BuildCommands( \
+               \ Id integer primary key unique not null,  \
+               \ Hash integer not null,                   \
                \ Command varchar(2048) unique not null)"
+        sqlIdx = "create index if not exists BuildCommandsHash on BuildCommands(Hash)"
+
+insertCommandSQL :: T.Text
+insertCommandSQL = "insert or ignore into BuildCommands (Command, Hash) values (?, ?)"
 
 -- Schema and operations for the BuildArgs table.
 defineBuildArgsTable :: Connection -> IO ()
-defineBuildArgsTable c = execute_ c sql
+defineBuildArgsTable c = execute_ c sql >> execute_ c sqlIdx
   where sql =  "create table if not exists BuildArgs(    \
                \ Id integer primary key unique not null, \
+               \ Hash integer not null,                  \
                \ Args varchar(2048) unique not null)"
+        sqlIdx = "create index if not exists BuildArgsHash on BuildArgs(Hash)"
+
+insertArgsSQL :: T.Text
+insertArgsSQL = "insert or ignore into BuildArgs (Args, Hash) values (?, ?)"
 
 -- Schema and operations for the SourceFiles table.
 defineSourceFilesTable :: Connection -> IO ()
 defineSourceFilesTable c = execute_ c sql
-  where sql =  "create table if not exists SourceFiles(                  \
-               \ File integer not null,                                  \
-               \ WorkingDirectory integer not null,                      \
-               \ BuildCommand integer not null,                          \
-               \ BuildArgs integer not null,                             \
-               \ LastBuilt integer zerofill unsigned not null,           \
-               \ foreign key(File) references Files(Id),                 \
-               \ foreign key(WorkingDirectory) references Paths(Id),     \
-               \ foreign key(BuildCommand) references BuildCommands(Id), \
-               \ foreign key(BuildArgs) references BuildArgs(Id),        \
+  where sql =  "create table if not exists SourceFiles(        \
+               \ File integer unique not null,                 \
+               \ WorkingDirectory integer not null,            \
+               \ BuildCommand integer not null,                \
+               \ BuildArgs integer not null,                   \
+               \ LastBuilt integer zerofill unsigned not null, \
                \ primary key(File))"
 
-updateSourceFile :: DBHandle -> CommandInfo -> IO ()
-updateSourceFile h (CommandInfo sf wd (Command cmd args) t) = do
-    --execute_ (conn h) "begin transaction"
-    fileId <- getFileId h sf
-    wdId <- getPathId h wd
-    cmdId <- getCommandId h cmd
-    argsId <- getArgsId h (T.intercalate " " args)
-    void $ withBind (updateSourceFileStmt h) (fileId, wdId, cmdId, argsId, t) $ voidNextRow (updateSourceFileStmt h)
-    --execute_ (conn h) "commit"
-
 updateSourceFileSQL :: T.Text
-updateSourceFileSQL = "replace into SourceFiles                                           \
+updateSourceFileSQL = "replace into SourceFiles                                     \
                       \(File, WorkingDirectory, BuildCommand, BuildArgs, LastBuilt) \
                       \values (?, ?, ?, ?, ?)"
 
-getFileId :: DBHandle -> T.Text -> IO Int64
-getFileId h v = do
-  existingId <- withBind (getFileStmt h) (Only v) $ nextRow (getFileStmt h)
-  case existingId of
-    Just (Only i) -> return i
-    _             -> withBind (insertFileStmt h) (Only v) (voidNextRow (insertFileStmt h))
-                  >> lastInsertRowId (conn h)
-
-getPathId :: DBHandle -> T.Text -> IO Int64
-getPathId h v = do
-  existingId <- withBind (getPathStmt h) (Only v) $ nextRow (getPathStmt h)
-  case existingId of
-    Just (Only i) -> return i
-    _             -> withBind (insertPathStmt h) (Only v) (voidNextRow (insertPathStmt h))
-                  >> lastInsertRowId (conn h)
-
-getCommandId :: DBHandle -> T.Text -> IO Int64
-getCommandId h v = do
-  existingId <- withBind (getCommandStmt h) (Only v) $ nextRow (getCommandStmt h)
-  case existingId of
-    Just (Only i) -> return i
-    _             -> withBind (insertCommandStmt h) (Only v) (voidNextRow (insertCommandStmt h))
-                  >> lastInsertRowId (conn h)
-
-getArgsId :: DBHandle -> T.Text -> IO Int64
-getArgsId h v = do
-  existingId <- withBind (getArgsStmt h) (Only v) $ nextRow (getArgsStmt h)
-  case existingId of
-    Just (Only i) -> return i
-    _             -> withBind (insertArgsStmt h) (Only v) (voidNextRow (insertArgsStmt h))
-                  >> lastInsertRowId (conn h)
-
-getKindId :: DBHandle -> T.Text -> IO Int64
-getKindId h v = do
-  existingId <- withBind (getKindStmt h) (Only v) $ nextRow (getKindStmt h)
-  case existingId of
-    Just (Only i) -> return i
-    _             -> withBind (insertKindStmt h) (Only v) (voidNextRow (insertKindStmt h))
-                  >> lastInsertRowId (conn h)
-
-getFileSQL, insertFileSQL :: T.Text
-getFileSQL = "select Id from Files where Name = ?"
-insertFileSQL = "insert into Files (Name) values (?)"
-
-getPathSQL, insertPathSQL :: T.Text
-getPathSQL = "select Id from Paths where Path = ?"
-insertPathSQL = "insert into Paths (Path) values (?)"
-
-getCommandSQL, insertCommandSQL :: T.Text
-getCommandSQL = "select Id from BuildCommands where Command = ?"
-insertCommandSQL = "insert into BuildCommands (Command) values (?)"
-
-getArgsSQL, insertArgsSQL :: T.Text
-getArgsSQL = "select Id from BuildArgs where Args = ?"
-insertArgsSQL = "insert into BuildArgs (Args) values (?)"
-
-getKindSQL, insertKindSQL :: T.Text
-getKindSQL = "select Id from Kinds where Kind = ?"
-insertKindSQL = "insert into Kinds (Kind) values (?)"
-
-mkQuery :: String -> Query
-mkQuery = fromString
-
-mkQueryT :: T.Text -> Query
-mkQueryT = mkQuery . T.unpack
+updateSourceFile :: DBHandle -> CommandInfo -> IO ()
+updateSourceFile h (CommandInfo sf wd (Command cmd args) t) = do
+    let sfHash = hash sf
+    execStatement h insertFileStmt (sf, sfHash)
+    let wdHash = hash wd
+    execStatement h insertPathStmt (wd, wdHash)
+    let cmdHash = hash cmd
+    execStatement h insertCommandStmt (cmd, cmdHash)
+    let argsJoined = T.intercalate " " args
+    let argsHash = hash argsJoined
+    execStatement h insertArgsStmt (argsJoined, argsHash)
+    execStatement h updateSourceFileStmt (sfHash, wdHash, cmdHash, argsHash, t)
 
 getAllSourceFiles :: DBHandle -> IO [CommandInfo]
 getAllSourceFiles h = query_ (conn h) sql
-  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt \
-              \ from SourceFiles                                           \
-              \ join Files as F on SourceFiles.File = F.Id                 \
-              \ join Paths as W on SourceFiles.WorkingDirectory = W.Id     \
-              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Id \
-              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Id"
+  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt           \
+              \ from SourceFiles                                             \
+              \ join Files as F on SourceFiles.File = F.Hash                 \
+              \ join Paths as W on SourceFiles.WorkingDirectory = W.Hash     \
+              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Hash \
+              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Hash"
 
 getCommandInfo :: DBHandle -> SourceFile -> IO (Maybe CommandInfo)
 getCommandInfo h sf = do
-    row <- query (conn h) sql (Only sf)
+    row <- query (conn h) sql (Only $ hash sf)
     return $ case row of
               (ci : _) -> Just ci
               _        -> Nothing
-  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt \
-              \ from SourceFiles                                           \
-              \ join Files as F on SourceFiles.File = F.Id                 \
-              \ join Paths as W on SourceFiles.WorkingDirectory = W.Id     \
-              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Id \
-              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Id        \
-              \ where F.Name = ? limit 1"
+  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt           \
+              \ from SourceFiles                                             \
+              \ join Files as F on SourceFiles.File = F.Hash                 \
+              \ join Paths as W on SourceFiles.WorkingDirectory = W.Hash     \
+              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Hash \
+              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Hash        \
+              \ where F.Hash = ? limit 1"
 
 -- Eventually this should be more statistical, but right now it will just
 -- return an arbitrary file from the same directory.
@@ -300,58 +246,63 @@ getSimilarCommandInfo h sf = do
     return $ case row of
               (ci : _) -> Just $ withSourceFile ci sf
               _        -> Nothing
-  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt \
-              \ from SourceFiles                                           \
-              \ join Files as F on SourceFiles.File = F.Id                 \
-              \ join Paths as W on SourceFiles.WorkingDirectory = W.Id     \
-              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Id \
-              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Id        \
+  where sql = "select F.Name, W.Path, C.Command, A.Args, LastBuilt           \
+              \ from SourceFiles                                             \
+              \ join Files as F on SourceFiles.File = F.Hash                 \
+              \ join Paths as W on SourceFiles.WorkingDirectory = W.Hash     \
+              \ join BuildCommands as C on SourceFiles.BuildCommand = C.Hash \
+              \ join BuildArgs as A on SourceFiles.BuildArgs = A.Hash        \
               \ where F.Name like ? limit 1"
 
 -- Schema and operations for the Kinds table.
 defineKindsTable :: Connection -> IO ()
-defineKindsTable c = execute_ c sql
+defineKindsTable c = execute_ c sql >> execute_ c sqlIdx
   where sql =  "create table if not exists Kinds(        \
                \ Id integer primary key unique not null, \
+               \ Hash integer not null,                  \
                \ Kind varchar(2048) unique not null)"
+        sqlIdx = "create index if not exists KindsHash on Kinds(Hash)"
+
+insertKindSQL :: T.Text
+insertKindSQL = "insert or ignore into Kinds (Kind, Hash) values (?, ?)"
 
 -- Schema and operations for the Definitions table.
 defineDefinitionsTable :: Connection -> IO ()
 defineDefinitionsTable c = execute_ c sql
-  where sql =  "create table if not exists Definitions(         \
-               \ Name varchar(2048) not null,                   \
-               \ USR varchar(2048) unique not null primary key, \
-               \ File integer not null,                         \
-               \ Line integer not null,                         \
-               \ Column integer not null,                       \
-               \ Kind integer not null,                         \
-               \ foreign key(File) references Files(Id),        \
-               \ foreign key(Kind) references Kinds(Id))"
+  where sql =  "create table if not exists Definitions(       \
+               \ USRHash integer primary key unique not null, \
+               \ Name varchar(2048) not null,                 \
+               \ USR varchar(2048) not null,                  \
+               \ File integer not null,                       \
+               \ Line integer not null,                       \
+               \ Column integer not null,                     \
+               \ Kind integer not null)"
 
 updateDef :: DBHandle -> DefInfo -> IO ()
 updateDef h (DefInfo n u (SourceLocation sf l c) k) = do
-    -- execute_ (conn h) "begin transaction"
-    fileId <- getFileId h sf
-    kindId <- getKindId h k
-    void $ withBind (updateDefStmt h) (n, u, fileId, l, c, kindId) $ voidNextRow (updateDefStmt h)
-    -- execute_ (conn h) "commit"
+    let usrHash = hash u
+    let sfHash = hash sf
+    execStatement h insertFileStmt (sf, sfHash)
+    let kindHash = hash k
+    execStatement h insertKindStmt (k, kindHash)
+    execStatement h updateDefStmt (usrHash, n, u, sfHash, l, c, kindHash)
 
 updateDefSQL :: T.Text
-updateDefSQL = "replace into Definitions                     \
-               \ (Name, USR, File, Line, Column, Kind) \
-               \ values (?, ?, ?, ?, ?, ?)"
+updateDefSQL = "replace into Definitions               \
+               \ (USRHash, Name, USR, File, Line, Column, Kind) \
+               \ values (?, ?, ?, ?, ?, ?, ?)"
 
 getDef :: DBHandle -> USR -> IO (Maybe DefInfo)
 getDef h usr = do
-    row <- query (conn h) sql (Only usr)
+    row <- query (conn h) sql (Only $ hash usr)
     return $ case row of
               (di : _) -> Just di
               _        -> Nothing
   where sql = "select D.Name, D.USR, F.Name, D.Line, D.Column, K.Kind \
-              \ from Definitions as D                                         \
-              \ join Files as F on D.File = F.Id                              \
-              \ join Kinds as K on D.Kind = K.Id                              \
-              \ where D.USR = ? limit 1"
+              \ from Definitions as D                                 \
+              \ join Files as F on D.File = F.Hash                    \
+              \ join Kinds as K on D.Kind = K.Hash                    \
+              \ where D.USRHash = ? limit 1"
   
 
 -- Checks that the database has the correct schema and sets it up if needed.
