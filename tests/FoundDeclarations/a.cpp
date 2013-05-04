e205fc423c5cca346b354e911d0a9cdc6be8daf2
Seth Fowler <mark.seth.fowler@gmail.com>
2013-05-04 14:06:03 -0700

    Add more test cases

--- tests/FoundDeclarations/a.cpp
+++ tests/FoundDeclarations/a.cpp
@@ -5,6 +5,16 @@ using namespace ns;
 #define CINST_MACRO cInstance
 #define ADD(x, y) x + y
 
+struct bStruct
+{
+  BClass b;
+};
+
+struct bPtrStruct
+{
+  BClass* b;
+};
+
 int main(int argc, char** argv)
 {
   BClass bInstance;
@@ -12,6 +22,12 @@ int main(int argc, char** argv)
   BClass* bPtr = new BClass;
   CClass* cPtr = new CClass;
   CClass* bInCPtr = bPtr;
+  bStruct bS;
+  bStruct* pBS = new bStruct;
+  bPtrStruct bPS;
+  bPS.b = bPtr;
+  bPtrStruct* pBPS = new bPtrStruct;
+  pBPS->b = bPtr;
 
   bInstance.CVirtualMethod(false);
   cInstance.CVirtualMethod(false);
@@ -19,6 +35,11 @@ int main(int argc, char** argv)
   cPtr->CVirtualMethod(false);
   bInCPtr->CVirtualMethod(false);
   bPtr->BasePureVirtualMethod();
+  bPtr->CClass::CVirtualMethod(false);
+  bS.b.CVirtualMethod(false);
+  pBS->b.CVirtualMethod(false);
+  bPS.b->CVirtualMethod(false);
+  pBPS->b->CVirtualMethod(false);
 
   return ADD(b, c) + bInstance.mB + CINST_MACRO.mC;
 }
