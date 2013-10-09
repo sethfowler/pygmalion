29641bf36e88fcadf3a22e6f184fb263f35f7c6a
Seth Fowler <mark.seth.fowler@gmail.com>
2013-10-09 14:06:47 -0700

    Add a new (ironically untested) test DSL. #118

--- tests/Pygmalion/Test.hs
+++ tests/Pygmalion/Test.hs
@@ -1,23 +1,99 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, LambdaCase #-}
+
 module Pygmalion.Test
 ( defShouldBe
 , defsShouldBe
 , index
+, line
+, PygmalionTest (..)
+, runPygmalionTest
+, Test (..)
+, test
 , withPygd
 ) where
 
 import Control.Concurrent (threadDelay)
 import Control.Exception (bracket)
 import Control.Monad (forM_, void)
+import Control.Monad.State (execState, modify, State)
+import Control.Monad.State.Class (MonadState)
 import Data.List (isInfixOf)
 import Data.Tuple.Curry (uncurryN)
-import System.IO (hGetContents)
+import System.IO (hGetContents, hPutStr, withFile, IOMode(..))
 import System.Process (CreateProcess(..), createProcess, runCommand, proc, StdStream(..), waitForProcess)
-import Test.Hspec (Expectation)
+import Test.Hspec (Expectation, pendingWith)
 import Test.HUnit (assertBool)
 
 import Pygmalion.Core (dbFile)
 
-defShouldBe :: (FilePath, Int, Int) -> String -> Expectation
+type Line = Int
+type Col = Int
+
+data Test = Def String
+          | Defs [String]
+          | Fails String
+            deriving (Eq, Show)
+
+data AnnotatedTest = DefTest Line Col [String]
+                   | FailingTest String
+                     deriving (Eq, Show)
+
+data TestDesc = TestDesc
+  { tsLines :: [String]
+  , tsTests :: [AnnotatedTest]
+  } deriving (Eq, Show)
+
+addDescLine :: String -> TestDesc -> TestDesc
+addDescLine s d = d { tsLines = (tsLines d) ++ [s] }
+
+addDescTest :: AnnotatedTest -> TestDesc -> TestDesc
+addDescTest t d = d { tsTests = (tsTests d) ++ [t] }
+
+newtype PygmalionTest a = PygmalionTest
+  { unPygmalionTest :: State TestDesc a
+  } deriving (Monad, MonadState TestDesc)
+
+line :: String -> PygmalionTest ()
+line s = modify (addDescLine s)
+
+test :: String -> [Test] -> PygmalionTest ()
+test s ts = do
+  let (finalLine, finalTests) = annotateTests 1 1 s ts
+  modify (addDescLine finalLine)
+  forM_ finalTests $ \t ->
+    modify (addDescTest t)
+
+annotateTests :: Line -> Col -> String -> [Test] -> (String, [AnnotatedTest])
+annotateTests _ _ [] [] = ([], [])
+annotateTests _ _ [] (x:_) = error $ "Test " ++ (show x) ++ " doesn't have a location"
+annotateTests l c ('@':_) [] = error $ "Location " ++ (show (l,c)) ++ " doesn't have a test"
+annotateTests l c ('@':xs) (t:ts) = let (xs', ts') = annotateTests l (c + 1) xs ts in
+                                    (xs', (annotateTest l (c + 1) t) : ts')
+annotateTests l c (x:xs) ts = let (xs', ts') = annotateTests l (c + 1) xs ts in
+                              ((x:xs'), ts')
+
+annotateTest :: Line -> Col -> Test -> AnnotatedTest
+annotateTest l c (Def s)   = DefTest l c [s]
+annotateTest l c (Defs ss) = DefTest l c ss
+annotateTest l c (Fails s) = FailingTest $ (show (l,c)) ++ ": " ++ s
+
+runPygmalionTest :: FilePath -> PygmalionTest () -> Expectation
+runPygmalionTest path t = do
+  let desc = execState (unPygmalionTest t) (TestDesc [] [])
+
+  -- Write test to a temporary file.
+  withFile path WriteMode $ \h ->
+    hPutStr h (unlines . tsLines $ desc)
+
+  -- Run the indexer.
+  index path
+
+  -- Run the tests.
+  forM_ (tsTests desc) $ \case
+    DefTest ln col ss -> (path, ln, col) `defsShouldBe` ss
+    FailingTest s     -> pendingWith s
+             
+defShouldBe :: (FilePath, Line, Col) -> String -> Expectation
 defShouldBe loc s = do
     ds <- uncurryN defsAt loc
     assertBool (errorMsg ds) $ length ds == 1
@@ -25,9 +101,10 @@ defShouldBe loc s = do
   where
     errorMsg ds = "Definition for " ++ show loc ++ " was " ++ show ds ++ "; expected " ++ show s
 
-defsShouldBe :: (FilePath, Int, Int) -> [String] -> Expectation
+defsShouldBe :: (FilePath, Line, Col) -> [String] -> Expectation
 defsShouldBe loc ss = do
     ds <- uncurryN defsAt loc
+    assertBool (errorMsg ds) $ length ds == length ss
     forM_ ss $ \s ->
       assertBool (errorMsg ds) $ any (s `isInfixOf`) ds
   where
@@ -54,8 +131,8 @@ index file = do
   void $ pygmalion ["index", "clang++", "--std=c++11", file]
   threadDelay 1000000
 
-defsAt :: FilePath -> Int -> Int -> IO [String]
-defsAt file line col = pygmalion ["definition", file, show line, show col]
+defsAt :: FilePath -> Line -> Col -> IO [String]
+defsAt file l c = pygmalion ["definition", file, show l, show c]
 
 sh :: String -> IO ()
 sh cmd = void $ waitForProcess =<< runCommand cmd
